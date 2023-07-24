fdce79ba599aa294aca2c1ad46bbdf65227647df
Michael Liu <micliu@ucdavis.edu>
2023-07-24 15:05:16 -0700

    editing boilerplate Co-authored-by: Garrett Woo garrettwoogs@yahoo.com Co-authored-by: Ulf Wong uwong8@gmail.com Co-authored-by: John Kim jayoo0621@gmail.com Co-authored-by: Justin Shim justinjaeshim@gmail.com

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="text-3xl font-bold underline">O</div>
+      <div className="text-3xl font-bold underline">OK</div>
     </div>
   );
 }
