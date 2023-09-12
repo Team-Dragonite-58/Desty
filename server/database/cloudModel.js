5267372cbfa03e904ea263043ed3e1d9ec29f92c
Ulf Wong <124991635+hkulf41@users.noreply.github.com>
2023-09-12 12:00:26 -0700

    Update cloudModel.js

--- server/database/cloudModel.js
+++ server/database/cloudModel.js
@@ -1,16 +1,10 @@
 import pkg from 'pg';
 const { Pool } = pkg;
-// const { Pool } = require('pg');
 
 const URI =
-  'postgres://vhelzmkc:xiuGjRmmcnbsmWgXxZ3DYK8_r2pj0ly6@mahmud.db.elephantsql.com/vhelzmkc';
+  '';
 
 const pool = new Pool({
-  // host: 'mahmud.db.elephantsql.com',
-  // user: 'fdmzhonk',
-  // password: '1UJxOe9TaNJBM9GMoEpkR-cAiWIQ2IeO',
-  // database: 'fdmzhonk',
-  // port: 5432,
   connectionString: URI,
 });
 
